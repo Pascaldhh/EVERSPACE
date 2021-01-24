c484ed9c40148ba77994ceec69802c4a376c1c5e
darkrage <darkrage6162@gmail.com>
2021-01-23 17:20:32 -0800

    planet.js test

---
+++ javascript/planet-inside.js
@@ -0,0 +1,8 @@
+var land=document.getElementById("land");
+var atmosphere=document.getElementById("atmosphere");
+
+function landclass(){
+	var landnumber = MATH.floor(MATH.random()*5+1);
+	alert(landnumber);
+}
+landclass();
