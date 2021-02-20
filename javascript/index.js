f765411c60a35aed3d07a41f1b0ec2e66b1103bb
darkrage <darkrage6162@gmail.com>
2021-02-20 10:07:19 -0800

    random planet styles added... -spaceship is now aligned. -planets are smaller than before. - basic Panel added.

--- javascript/index.js
+++ javascript/index.js
@@ -2,6 +2,8 @@ function spaceshipSize() {
 	let ship = document.querySelector('#spaceship');
 	ship.style.width=window.innerWidth/6.66666666+"px";
 	ship.style.height=window.innerWidth/12+"px";
+	ship.style.left=window.innerWidth/2-ship.offsetWidth/2+"px";
+	ship.style.top=window.innerHeight/2-ship.offsetHeight/2+"px";
 }
 function planetSize() {
 	let world = document.querySelector('#planet');
@@ -18,7 +20,7 @@ function healthbarSize() {
 var ship = document.querySelector('#spaceship');
 var planet1 = document.querySelector("#planet");
 var planetX = window.innerWidth;
-var planetY = 0;
+var planetY = 50;
 let speed = 40;
 function movements(e) {
 	
@@ -34,12 +36,37 @@ document.onkeydown = movements;
 //  RANDOM SPWNING OF PLANET AND OTHER STUFF.
 function randomPlanet() {
 	let planetleft="-"+window.innerWidth*2;
+	let planetright=window.innerWidth*2;
+	//	LEFT
 	if (planetX<=planetleft) {
 		planetX=window.innerWidth+50;
 		planet1.style.left=planetX+"px";
+        let number_for_planet=Math.floor(Math.random() * 50)+1;
+        planet1.className ="planetstyle"+number_for_planet;
+	}
+	//	RIGHT
+	else if (planetX>=planetright) {
+		planetX=window.innerWidth+50;
+		planet1.style.left="10px";
+        let number_for_planet=Math.floor(Math.random() * 50)+1;
+        planet1.className ="planetstyle"+number_for_planet;
 	}
 }
+
+/*	THINGS THAT SHOULD BE DONE ONLY WHEN WEBSITE LOADS	*/
+
+    let number_for_planet=Math.floor(Math.random() * 50)+1;
+    planet1.className ="planetstyle"+number_for_planet;
 
 planetSize();
 spaceshipSize();
 healthbarSize();
+
+
+
+
+// 						TEST ---->>
+
+let plat = window.innerHeight;
+let panel=document.querySelector("#panel");
+panel.style.top = plat-150+"px";
