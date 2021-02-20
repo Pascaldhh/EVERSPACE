289f787d967dc74dc56479d72d2d2f03127d30d1
darkrage <darkrage6162@gmail.com>
2021-02-20 04:42:22 -0800

    basic spawning of planet. (spaceship is not well aligned) 50 empty classes in index.css file.

--- javascript/index.js
+++ javascript/index.js
@@ -8,21 +8,38 @@ function planetSize() {
 	world.style.width=window.innerWidth/8+"px";
 	world.style.height=window.innerWidth/8+"px";
 }
-
+function healthbarSize() {
+	let healthbar = document.querySelector("#healthbar");
+	let health = document.querySelector("#health");
+	healthbar.style.width = window.innerWidth/7+"px";
+	health.style.width = window.innerWidth/7.5+"px";
+}
 //    MOVEMENTS
 var ship = document.querySelector('#spaceship');
 var planet1 = document.querySelector("#planet");
 var planetX = window.innerWidth;
 var planetY = 0;
-let speed = 5;
+let speed = 40;
 function movements(e) {
 	
-         if(e.keyCode==68){ planetX-=speed; planet1.style.left=planetX+"px"; ship.style.transform = "scaleX(1)";}
+         if(e.keyCode==68){ planetX-=speed; planet1.style.left=planetX+"px"; ship.style.transform = "scaleX(1)"; }
     else if(e.keyCode==65){ planetX+=speed; planet1.style.left=planetX+"px"; ship.style.transform = "scaleX(-1)";}
     else if(e.keyCode==87){ planetY+=speed; planet1.style.top=planetY+"px"; }
     else if(e.keyCode==83){ planetY-=speed; planet1.style.top=planetY+"px"; }
+    randomPlanet();
 }
 planet1.style.left=planetX+"px";
 document.onkeydown = movements;
+
+//  RANDOM SPWNING OF PLANET AND OTHER STUFF.
+function randomPlanet() {
+	let planetleft="-"+window.innerWidth*2;
+	if (planetX<=planetleft) {
+		planetX=window.innerWidth+50;
+		planet1.style.left=planetX+"px";
+	}
+}
+
 planetSize();
 spaceshipSize();
+healthbarSize();
