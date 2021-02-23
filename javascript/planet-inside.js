ac37a72fd963003ef38d83bb93587d603771e8da
darkrage <darkrage6162@gmail.com>
2021-02-23 03:19:39 -0800

    Player added...(READ MORE) -land and atmosphere are aligned well. -land and atmosphere will now rotate on keys.(left, right) -some land designs changed.

--- javascript/planet-inside.js
+++ javascript/planet-inside.js
@@ -29,8 +29,8 @@ function atmosphereclass(){
 	else if(atmospherenumber==6){ sky.classList.add("atmosphere6"); }
 	else if(atmospherenumber==7){ sky.classList.add("atmosphere7"); }
 	else if(atmospherenumber==8){ sky.classList.add("atmosphere8"); }
-	sky.style.width=window.innerWidth+window.innerWidth/5+"px";
-	sky.style.height=window.innerWidth+window.innerWidth/5+"px";
+	sky.style.width=window.innerWidth+window.innerWidth/8+"px";
+	sky.style.height=window.innerWidth+window.innerWidth/8+"px";
 }
 
 /* 
@@ -38,8 +38,43 @@ function atmosphereclass(){
  page is loaded.... we will modify it later, cuz we dont want someone to 
  collect all the items and then reload the page to get more.
 */
+
+/*	 AlIGNING	*/
+function alignLand() {
+	var land=document.querySelector("#land");
+	land.style.left=window.innerWidth/2-land.offsetWidth/2+"px";
+	land.style.top=window.innerHeight/2+"px";
+}
+function alignAtmosphere() {
+	var atmosphere=document.querySelector("#atmosphere");
+	atmosphere.style.left=window.innerWidth/2-atmosphere.offsetWidth/2+"px";
+	atmosphere.style.top=window.innerHeight-atmosphere.offsetWidth/2.55+"px";
+}
+
+/*	 MOVEMENTS	*/
+let atmosphere1=document.querySelector("#atmosphere");
+let land1=document.querySelector("#land");
+let moveright=-1;
+let moveleft=1;
+function move(e) {
+	// D
+	if (e.keyCode==68){
+		land1.style.transform="rotate("+moveright+"deg)";
+		atmosphere1.style.transform="rotate("+moveright+"deg)";
+		moveright-=1;
+	}
+	// A
+	if (e.keyCode==65){
+		land1.style.transform="rotate("+moveleft+"deg)";
+		atmosphere1.style.transform="rotate("+moveright+"deg)";
+		moveleft+=1;
+	}
+}
+document.onkeydown=move;
+/*	 CALLING THE FUNCTIONS	*/
 landclass();
 atmosphereclass();
-
+alignAtmosphere();
+alignLand();
 
 /*	END OF THE SCRIPT	*/
